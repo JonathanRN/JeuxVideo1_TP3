370800644c9a18b21ad9963dbcf1f2adaada0881
kevinleclaire <kevinleclaire3@hotmail.com>
2016-11-25 10:40:10 -0500

    couleur

--- TP3Etu/TP3/Form1.cs
+++ TP3Etu/TP3/Form1.cs
@@ -4,9 +4,9 @@ using System.Windows.Forms;
 
 namespace TP3
 {
-  public partial class FormPrincipale : Form
+  public partial class Form1 : Form
   {
-    public FormPrincipale( )
+    public Form1( )
     {
       InitializeComponent( );
     }
@@ -27,7 +27,6 @@ namespace TP3
       // Ne pas oublier de mettre en place les valeurs nécessaires à une partie.
       ExecuterTestsUnitaires();
       InitialiserSurfaceDeJeu(20,10);
-            ChangerPictureBoxCouleur();
     }
 
     private void InitialiserSurfaceDeJeu(int nbLignes, int nbCols)
@@ -87,13 +86,8 @@ namespace TP3
       
       // Clean-up
     }
-        void ChangerPictureBoxCouleur()
-        {
-            toutesImagesVisuelles[1, 1].BackColor = Color.Red;
-        }
 
     #endregion
-
 
   }
 
