8dac7362dac3fd17359bc8cc8876a144a144ac88
JonathanRN <minijona7@gmail.com>
2016-12-03 10:30:58 -0500

    son

--- ProjetExempleSon/ProjetExempleSon/Properties/AssemblyInfo.cs
+++ ProjetExempleSon/ProjetExempleSon/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Son")]
+[assembly: AssemblyTitle("ProjetExempleSon")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Son")]
+[assembly: AssemblyProduct("ProjetExempleSon")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("78619a3d-f971-4efb-bb53-9696aabe77bf")]
+[assembly: Guid("a217ccae-b151-4fb4-b172-da4cd6eae89c")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
